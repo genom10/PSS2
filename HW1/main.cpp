6fa0f5ca1bb46270e38a26ab4e43407195992742
George <genom_10@mail.ru>
2021-02-10 22:13:42 +0300

    Comments and beautifying

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -11,12 +11,12 @@ bool caseInsensetiveComporator (char a, char b){
     return a == b;
 }
 
-bool checkForWord(char *text, char (&request)[100]) {
+bool checkForWord(char *text, char (&request)[100]) { //compares word at the beginning of the text and request
     for (int i = 0; request[i] != '\000'; i++){
         if (!caseInsensetiveComporator(text[i], request[i]))
-            return false;
+            return false; //stop if found difference
     }
-    return true;
+    return true; //no differences. words are matched
 }
 
 char toUpper(char a){
@@ -25,43 +25,41 @@ char toUpper(char a){
 
 void printAllMatches(char (&text)[5001], char (&request)[100]) {
     int sentenceNumber = 1;
-    int sentencePos[100];
+    int sentencePos[100];//positions of sentences' starts
     sentencePos[0] = 0;
     int matchesNumber = 0;
-    int matchesPos[50];
+    int matchesPos[50];//positions of words matching 'request'
     int i;
     for (i = 0; text[i] != '\000'; i++){
-        if(checkForWord(text, request)){
+        if(checkForWord(text, request))
             matchesPos[matchesNumber++] = i; //check first word
-        }
 
-        if (text[i] == '.'){
-            sentencePos[sentenceNumber++] = i + 2;
-        }
+        if (text[i] == '.')
+            sentencePos[sentenceNumber++] = i + 2; //record position of sentence's start
 
         if (text[i] == ' '){ //searching for matches after each space
             if(checkForWord(text+i+1, request)){
-                matchesPos[matchesNumber++] = i+1;
+                matchesPos[matchesNumber++] = i+1; // record position of word matching 'request'
                 //printf("%i ", i);
             }
         }
     }
-    sentencePos[sentenceNumber] = i;
+    sentencePos[sentenceNumber] = i; // last sentence's end
 
     printf("%i\n", matchesNumber);
     fprintf(outp, "%i\n", matchesNumber);
 
-    for (int i = 0, j = 0; i < sentenceNumber; i++) {
+    for (int i = 0, j = 0; i < sentenceNumber; i++) {//loop through sentences
         if (j >= matchesNumber) // no more matches
             return;
         if (sentencePos[i+1] > matchesPos[j] ) { // if there's a match in this sentence
             bool upper = false;
             for (int k = sentencePos[i]; text[k] != '.'; k++) {
-                if (k == matchesPos[j]){
+                if (k == matchesPos[j]){ //this word matches
                     upper = true;
                     j++;
                 }
-                if (text[k] == ' '){
+                if (text[k] == ' '){ //end of [THE WORLD]
                     upper = false;
                 }
                 printf("%c", upper ? toUpper(text[k]) : text[k]);
